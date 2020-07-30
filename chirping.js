9d54e2995fb28584ba213d61085383bc2eb0b15d
hemsleek <asiyanbimubashir@gmail.com>
2020-07-30 23:59:15 +0100

    WIP: login form styling

---
+++ chirping.js
@@ -0,0 +1,11 @@
+/* setup and initialization */
+
+
+/*selectors*/
+const inputElement = document.getElementsByClassName('label');
+const labelElement = document.getElementsByClassName('input-field');
+
+/*utility functions*/
+
+
+/*event listeners and triggers*/ 
