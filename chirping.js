3a0ab9f8600551f5b41ef37163ff8457e3a0e8d4
hemsleek <asiyanbimubashir@gmail.com>
2020-08-14 23:55:20 +0100

    fix: triggers

--- chirping.js
+++ chirping.js
@@ -6,23 +6,34 @@ const inputElement = document.getElementById('input-field');
 const inputElement2 = document.getElementById('input-field2');
 const labelElement = document.getElementById('label');
 const labelElement2 = document.getElementById('label2');
+const formElement = document.getElementById("form-element")
 
 /*utility functions*/
 
 
-/*event listeners and triggers*/ 
-inputElement.addEventListener('focus', e => {
-    inputElement.classList.add('focus');
-    labelElement.classList.add('caption');
-})
-inputElement2.addEventListener('focus', e => {
-    inputElement2.classList.add('focus');
-    labelElement2.classList.add('caption');
-})
-inputElement.addEventListener('focusout', e => {
-    inputElement.classList.remove('focus');
-    labelElement.classList.remove('caption');
-})
+/*event listeners and triggers*/
+formElement.addEventListener('focus', e => {
+        if (!e.target.dataset.select) return;
+
+        else {
+
+            if (e.target.dataset.select == "input1") {
+
+                e.target.classList.add('focus');
+                labelElement.classList.add('caption');
+
+            } else if (e.target.dataset.select == "input2") {
+                e.target.classList.add('focus');
+                labelElement2.classList.remove('caption');
+
+            }
+        }
+
+        // inputElement.classList.add('focus');
+        // labelElement.classList.add('caption');
+    })
+    // inputElement2.addEventListener('focus', e => {})
+inputElement.addEventListener('focusout', e => {})
 inputElement2.addEventListener('focusout', e => {
     inputElement2.classList.remove('focus');
     labelElement2.classList.remove('caption');
